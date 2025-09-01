dbfe35a9024cab392532dce12dc7241ce388342f
cl101rick <cl101rick@yahoo.com>
2025-09-01 15:02:14 -0700

    go

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ OUTPUT_FOLDER = '/app/outputs'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
+def home():
+    return ''hello world!''
+    <!doctype html>
+    <title>Upload PDF for MusicXML Conversion</title>
+    <h1>Upload PDF File</h1>
+    <form method=post enctype=multipart/form-data action="/convert">
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    
+
+
 @app.route('/convert', methods=['POST'])
 def convert_pdf():
     # Handle file upload
